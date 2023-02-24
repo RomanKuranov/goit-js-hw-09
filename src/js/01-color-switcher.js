bca81ee7f6aad6776c95fb5cbdb5f7e4f8666916
RomanKuranov <romankuranov93@gmail.com>
2023-02-24 17:54:46 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const onStartClick = (event) => {
+   timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startBtn.toggleAttribute("disabled"); 
+}
+
+const onStopClick = (event) => {
+clearInterval(timerId);
+startBtn.removeAttribute("disabled"); 
+}
+
+startBtn.addEventListener("click", onStartClick);
+stopBtn.addEventListener("click", onStopClick);
+
+
+
+
+
+
+
+
+
+
+
 
