6311f146684915c7b9f2aca7d3481ddd27c93b97
RomanKuranov <romankuranov93@gmail.com>
2023-02-25 17:33:31 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let delay = +event.currentTarget.elements.delay.value;
+  let step = +event.currentTarget.elements.step.value;
+  let amount = +event.currentTarget.elements.amount.value;
+ 
+
+  for (let i =1; i <= amount; i += 1) {
+    createPromise(i,delay).then(({position, delay})=>{
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({position, delay})=>{
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+}
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+  return new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    if (shouldResolve) {
+      resolve({position, delay})
   } else {
-    // Reject
+    reject({position, delay})
   }
+}, delay)
+  })
 }
